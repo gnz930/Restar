f78f9507e00fdeea00bb9402998a9357ee5a726a
k8ykn <yusuke@verid.cc>
2025-12-28 16:22:42 +0900

    Add Xcode project

---
+++ Color/Color/ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorApp.swift
+//  Color
+//
+//  Created by Yusuke Otsuru on 2025/12/28.
+//
+
+import SwiftUI
+
+@main
+struct ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color/Color/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Color
+//
+//  Created by Yusuke Otsuru on 2025/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
